037009e7f7e3172bb069fb2e4072f2774e531dc3
Chu A. <chu.amk22@gmail.com>
2025-09-25 16:59:48 +0100

    feat: worked on three features; click numbers(operands), toggle scientific calc, clear the screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const advanceBtn = document.querySelector('.scientific')
+const calcAdvance = document.querySelector('.calc-advance')
+const calcBtn = document.querySelector('.calc-btns')
+const clearBtn = document.querySelector('.clear')
+
+// select input and output screens
+let inputScreen = document.querySelector('.input')
+let outputScreen = document.querySelector('.output')
+
+// select all the buttons
+const operandBtn = document.querySelectorAll('.operand')
+console.log(operandBtn)
+
+// listen to click events on all numbers
+for (const operand of operandBtn) {
+  operand.addEventListener('click', () => {
+    inputScreen.textContent += operand.textContent
+  })
+}
+
+// toggle scientific calculator
+advanceBtn.addEventListener('click', () => {
+  calcAdvance.classList.toggle('hidden')
+  calcAdvance.style.paddingLeft = '0px'
+})
+
+// clear the screen
+clearBtn.addEventListener('click', () => {
+  inputScreen.textContent = ''
+  outputScreen.textContent = ''
+})
